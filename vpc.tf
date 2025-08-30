00290ac15cd6dc9831a1e43b7bd962ac9ab31ac2
chaitanya <chaitanya@gmail.com>
2025-08-30 07:49:46 +0530

    added igw code

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,13 @@ resource "aws_vpc" "testvpc" {
   cidr_block           = "10.100.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    "key" = "SL-vpc"
+    "Name" = "SL-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "testigw" {
+  vpc_id = aws_vpc.testvpc.id
+  tags = {
+    "Name"="SL-IGW"
   }
 }
