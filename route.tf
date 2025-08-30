da617e39bcf3f99b316a79550ce3f2460fe05371
chaitanya <chaitanya@gmail.com>
2025-08-30 20:12:52 +0530

    added private route table

--- route.tf
+++ route.tf
@@ -8,3 +8,10 @@ resource "aws_route_table" "testroute" {
     gateway_id = aws_internet_gateway.testigw.id
   }
 }
+
+resource "aws_route_table" "testroutepvt" {
+  vpc_id = aws_vpc.testvpc.id
+  tags = {
+    "Name"="SL-Route-Private"
+  }
+}
