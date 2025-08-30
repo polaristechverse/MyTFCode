5a898e2fa90015f95762e6cafa6ccaba6a845840
chaitanya <chaitanya@gmail.com>
2025-08-30 20:11:02 +0530

    added route.tf file

---
+++ route.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "testroute" {
+  vpc_id = aws_vpc.testvpc.id
+  tags = {
+    "Name"="SL-route"
+  }
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.testigw.id
+  }
+}
