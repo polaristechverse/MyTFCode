10d2a238e4d7a0e69c457bb387135c14c25b9a5f
chaitanya <chaitanya@gmail.com>
2025-09-03 07:18:07 +0530

    added new subnet3

--- vpc.tf
+++ vpc.tf
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "testigw" {
 resource "aws_subnet" "testsubnet1" {
   vpc_id                  = aws_vpc.testvpc.id
   cidr_block              = "10.100.1.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags = {
     "Name" = "sl-subnet-1"
@@ -24,10 +24,19 @@ resource "aws_subnet" "testsubnet1" {
 resource "aws_subnet" "testsubnet2" {
   vpc_id                  = aws_vpc.testvpc.id
   cidr_block              = "10.100.2.0/24"
-  availability_zone       = "us-east-1"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
     "Name" = "sl-subnet-2"
   }
 }
 
+resource "aws_subnet" "testsubnet3" {
+  vpc_id                  = aws_vpc.testvpc.id
+  cidr_block              = "10.100.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+  tags = {
+    "Name" = "sl-subnet-3"
+  }
+}
