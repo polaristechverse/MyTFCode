1f7acf2e8d41b5ccb3b33d5cd30d0969cec26cd1
chaitanya <chaitanya@gmail.com>
2025-09-03 07:34:24 +0530

    added subnet 2 to private route

--- route.tf
+++ route.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "testroute" {
   vpc_id = aws_vpc.testvpc.id
   tags = {
-    "Name" = "SL-route"
+    "Name"="SL-route"
   }
-  route {
+  route  {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.testigw.id
   }
@@ -12,11 +12,16 @@ resource "aws_route_table" "testroute" {
 resource "aws_route_table" "testroutepvt" {
   vpc_id = aws_vpc.testvpc.id
   tags = {
-    "Name" = "SL-Route-Private"
+    "Name"="SL-Route-Private"
   }
 }
 
 resource "aws_route_table_association" "testpublic_route_association" {
   route_table_id = aws_route_table.testroute.id
-  subnet_id      = aws_subnet.testsubnet1.id
+  subnet_id = aws_subnet.testsubnet1.id
+}
+
+resource "aws_route_table_association" "testpublic_route_association_private" {
+  route_table_id = aws_route_table.testroutepvt.id
+  subnet_id = aws_subnet.testsubnet2.id
 }
